746e16191eee60692ae1ab9777e75cdcac98c960
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2024-10-10 22:14:33 +0530

    feat: Changed the namespace api url

--- api/controllers/controller.go
+++ api/controllers/controller.go
@@ -139,7 +139,7 @@ func listNamespaces() string {
 		return "Error Occured\n"
 	}
 
-	cmd := exec.Command("curl", "--cacert", cacert, "--header", "Authorization: Bearer "+string(token), apiserver+"/api/v1/namespaces | jq '.items[].metadata.name'")
+	cmd := exec.Command("curl", "--cacert", cacert, "--header", "Authorization: Bearer "+string(token), apiserver+"/api/v1/namespaces")
 
 	out, err := cmd.Output()
 	
