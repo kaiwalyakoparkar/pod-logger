3cbe8bf6c876bad01fd13afd02d417c7eaa74b99
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2024-10-12 13:38:43 +0530

    fix: shifted from jq to grep

--- api/controllers/controller.go
+++ api/controllers/controller.go
@@ -139,7 +139,7 @@ func listNamespaces() string {
 		return "Error Occured\n"
 	}
 
-	cmd := exec.Command("curl", "--cacert", cacert, "--header", "Authorization: Bearer "+string(token), apiserver+"/api/v1/namespaces | jq -r '.items[].metadata.name'")
+	cmd := exec.Command("curl", "--cacert", cacert, "--header", "Authorization: Bearer "+string(token), apiserver+"/api/v1/namespaces| grep -o '\"name\": \"[^\"]*' | grep -o '[^\"]*$'")
 
 	out, err := cmd.Output()
 	
